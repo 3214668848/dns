c8d318742f6c4555fa7daf5abcfdfeabb0657d5f
3214668848 <3214668848@163.com>
2011-07-06 16:57:58 +0800

    fixed some problem

--- dnstest.c
+++ dnstest.c
@@ -6,7 +6,7 @@ int main(){
     dns_serv.sin_family=AF_INET;
     dns_serv.sin_port=htons(53);
     //dns_serv.sin_addr.s_addr=inet_addr("211.87.226.11");
-	dns_serv.sin_addr.s_addr=inet_addr("8.8.8.8");
+	dns_serv.sin_addr.s_addr=inet_addr("211.87.232.227");
     int sk=socket(AF_INET,SOCK_DGRAM,0);
     struct sockaddr_in addr;
     addr.sin_family=AF_INET;
@@ -18,14 +18,21 @@ int main(){
     }
 	printf("bind success\n");
 	struct udp_packet *p=(struct udp_packet *)malloc(sizeof(struct udp_packet));
-	dns_query_package(p,"www.sc.sdu.edu.cn\0",0);
-   	int ret=sendto(sk,&p->dns_hdr,p->len,0,(struct sockaddr*)&dns_serv,sizeof(addr));
-	printf("send %d\n",ret);
+	dns_query_package(p,"www.baidu.com\0",0);
+	int ret=0,i=0;
+	for(;i<5000;++i){
+   		ret=sendto(sk,&p->dns_hdr,p->len,0,(struct sockaddr*)&dns_serv,sizeof(addr));
+		printf("i=%d send %d\n",i,ret);
+	}
 	memset(p,0,sizeof(struct udp_packet));
 	int size=sizeof(addr);
-	int recv_len=recvfrom(sk,&p->dns_hdr,sizeof(struct udp_packet),0,(struct sockaddr*)&addr,&size);
-	char *a1,*a2;
-	printf("%s %d\n",inet_ntoa(p->src_ip),p->src_port);
-	decode(p,a1,a2);
+	int rret=0;
+	for(i=0;i<5000;++i){
+		rret=recvfrom(sk,&p->dns_hdr,sizeof(struct udp_packet),0,(struct sockaddr*)&addr,&size);
+		printf("%d\n",i);
+		char *a1,*a2;
+		printf("%s %d\n",inet_ntoa(p->src_ip),p->src_port);
+		decode(p,a1,a2);
+	}
 	return 0;
 }
